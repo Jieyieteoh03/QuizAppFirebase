c5789afdf0cf304dda7b50f78d70738ea90a428b
Teoh Jie Yie <jieyiet2003@gmail.com>
2024-08-21 14:58:57 +0800

    update auth

--- app/src/main/java/com/jy/mobstarterapp/core/di/Utils.kt
+++
@@ -1,9 +0,0 @@
-package com.jy.mobstarterapp.core.di
-
-import android.util.Log
-
-object Utils {
-    fun debugLog (tag: String = "debugging"): (Any) -> Unit {
-        return { Log.d(tag, it.toString()) }
-    }
-}
